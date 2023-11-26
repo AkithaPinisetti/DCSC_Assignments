a14460ae8ce631a151807aadb431f5c3c75bc8d1
AkithaPinisetti2107 <akpi9370@colorado.edu>
2023-11-25 20:42:47 -0700

    sdfs

--- Lab3/Solution/dags/outcomes_dag.py
+++ Lab3/Solution/dags/outcomes_dag.py
@@ -5,11 +5,14 @@ from airflow.operators.python import PythonOperator
 
 from datetime import datetime 
 import os
-from etl_scripts.transform import transform_data
+
+from etlscripts.transform import transform_data
+
 SOURCE_URL = 'https://data.austintexas.gov/api/views/9t4d-g238/rows.csv'
 AIRFLOW_HOME = os.environ.get('AIRFLOW_HOME', '/opt/airflow')
 CSV_TARGET_DIR = AIRFLOW_HOME + '/data/{{ ds }}/downloads'
-CSV_TARGET_FILE = CSV_TARGET_DIR+'outcomes_{{ ds }}.csv'
+CSV_TARGET_FILE = CSV_TARGET_DIR+'/outcomes_{{ ds }}.csv'
+PQ_TARGET_DIR = AIRFLOW_HOME+'/data/{{ ds }}/processed'
 
 with DAG(
     dag_id="outcomes_dag",
@@ -20,14 +23,16 @@ with DAG(
 
     step1 = BashOperator(
         task_id="step1",
-        bash_command=f"curl -create-dirs -o {CSV_TARGET_FILE} {SOURCE_URL}",
+        bash_command="pwd"
+        # bash_command=f"curl -create-dirs -o {CSV_TARGET_FILE} {SOURCE_URL}",
     )
 
     step2 = PythonOperator(
         task_id="transform",
         python_callable = transform_data,
         op_kwargs = {
-            'source_csv' : CSV_TARGET_FILE
+            'source_csv' : CSV_TARGET_FILE,
+            'target+dir' : PQ_TARGET_DIR
         }
     )
 
