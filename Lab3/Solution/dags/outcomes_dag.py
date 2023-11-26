321db658f3dccf5a05971421056e1ed3de805d13
AkithaPinisetti2107 <akpi9370@colorado.edu>
2023-11-25 21:49:17 -0700

    sdf

--- Lab3/Solution/dags/outcomes_dag.py
+++ Lab3/Solution/dags/outcomes_dag.py
@@ -11,7 +11,7 @@ from etlscripts.transform import transform_data
 SOURCE_URL = 'https://data.austintexas.gov/api/views/9t4d-g238/rows.csv'
 AIRFLOW_HOME = os.environ.get('AIRFLOW_HOME', '/opt/airflow')
 CSV_TARGET_DIR = AIRFLOW_HOME + '/data/{{ ds }}/downloads'
-CSV_TARGET_FILE = CSV_TARGET_DIR+'/outcomes_{{ ds }}.csv'
+CSV_TARGET_FILE = CSV_TARGET_DIR + '/outcomes_{{ ds }}.csv'
 PQ_TARGET_DIR = AIRFLOW_HOME+'/data/{{ ds }}/processed'
 
 with DAG(
@@ -23,7 +23,8 @@ with DAG(
 
     step1 = BashOperator(
         task_id="step1",
-        bash_command=f"curl -create-dirs -o {CSV_TARGET_FILE} {SOURCE_URL}",
+        bash_command = "ls -ld"
+        # bash_command=f"curl -create-dirs -o {CSV_TARGET_FILE} {SOURCE_URL}",
     )
 
     step2 = PythonOperator(
